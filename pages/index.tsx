b8b55362756b13499e08f1e2d4c14234d4db5e77
Kio Nguyen <vanhungtbt1998@gmail.com>
2023-03-28 17:25:02 +0700

    chore: Add MUI

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,11 +3,12 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 
-const inter = Inter({ subsets: ['latin'] })
+import Button from "@mui/material/Button";
 
 export default function Home() {
   return (
     <>
+    <Button variant="contained">Hello World</Button>;
      <h1>Home Page</h1>
     </>
   )
